02c66b16614d4644c9e29e7e2a15ecaaed708b3d
“Aditya <adityajdj.gupta@gmail.com>
2018-06-23 20:52:12 +0530

    log file

--- logs.php
+++ logs.php
@@ -1,14 +1,23 @@
-<?php include("logs.txt"); ?>
 <?php
-   function logToFile($filename, $msg)
-   {
-   // open file
-   $fd = fopen($filename, "logs.txt");
-   // append date/time to message
-   $str = "[" . date("Y/m/d h:i:s", mktime()) . "] " . $msg;
-   // write string
-   fwrite($fd, $str . "\n"); 
-   // close file
-   fclose($fd);
-   }
-   ?>
+
+if(isset($_POST['login_user'])){
+$user = $_SERVER['login_user'];
+$filename ="";
+$msg="";
+// open a file
+$file = fopen($filename,"logs.txt");
+// append date/time to message
+$str = "[" . date("Y/m/d h:i:s", mktime()) . "] " . $msg;
+
+// write string
+fwrite($file,$user,$str ."\n");
+echo "Your user has been logged";
+fclose($file);
+}else{
+  echo "Not logged";
+}
+
+
+
+
+?>
